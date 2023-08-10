7ced9fe0756e13b236ab6877507bc2641dffe20e
Ajit Bhosale <AjitBhosale@eaton.com>
2023-08-10 16:32:56 +0530

    Initial Code Commit

---
+++ DpsDeviceRegistration/DpsDeviceRegistration/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+
+namespace DpsDeviceRegistration
+{
+	internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var globalDeviceEndpoint = "global.azure-devices-provisioning.net";
+            
+            // ToDo: following 3 details needs to be update as per device registration API response.
+            var deviceRegistrationId = "";
+            var idScope = "";
+            var deviceDerivedKey = "";
+
+            var securityProvider = new SecurityProviderSymmetricKey(deviceRegistrationId, deviceDerivedKey, deviceDerivedKey);
+
+            var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly);
+
+            var client = ProvisioningDeviceClient.Create(globalDeviceEndpoint, idScope, securityProvider, transport);
+
+
+            var deviceRegistrationResult = await client.RegisterAsync();
+
+            Console.WriteLine($"Provisioning result: {deviceRegistrationResult.Status}");
+
+            if (deviceRegistrationResult.Status != ProvisioningRegistrationStatusType.Assigned)
+            {
+                throw new InvalidOperationException("Something went wrong while trying to provision.");
+            }
+
+            if (!string.IsNullOrEmpty(deviceRegistrationResult.AssignedHub))
+            {
+                Console.WriteLine($"Assigned to hub : {deviceRegistrationResult.AssignedHub}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
